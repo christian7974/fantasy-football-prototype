42e19c23076736e4e9c8b7bba83c560dc452cfb2
christian7974 <ctropeano@uri.edu>
2024-09-12 14:40:46 -0400

    Initial Commit
    Using APIs to get the teams, players on each team.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default function Home() {
   axios.defaults.withCredentials = true;
-  const leagueId = String(1971867921);
+  const leagueId = process.env.LEAGUE_ID;
   const year = String(2024);
 
   const getTeamsRosterURL = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/" + year + "/segments/0/leagues/" + leagueId + "?view=mRoster";
   const getAllTeamsURL = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/" + year + "/segments/0/leagues/" + leagueId + "?view=mTeam"
 
-  const cookies = "swid={9F26388E-C1B1-425E-A638-8EC1B1125E74}; espn_s2=AECjeRPMXZVD7IcPqBn1JA%2BvGXc09IQReFgr4uuTGOWx2bp%2FNVlGs53HKI1aE3KmzzxL2wSs%2BJXVvC73xLWbBWPtM2zkqmBp3Mp9m0E4ogdnX31Y5V5KDuci3MWTCN6IO0ABW8iINwr1TsAFsIm6xA4E8I8DcUHW0YAlKPf1t9M5xMJPejb0WyDcNqCC2ulxWufGCAeQoFe3u7HZNSx99Hx9AjrywP6n6AoEK7gD1TeDPK7B9SVAVQLG85VNsdP1WPbGB5wwW3enkbUmTDvLYk5IDa%2FwkwHtITmgIM6SohAVXQ%3D%3D";
+  const cookies = `swid={${process.env.SWID}}; espn_s2=${process.env.ESPN_S2}`;
 
   const headers = {
     'Cookie': cookies
@@ -35,7 +35,7 @@ export default function Home() {
       // console.log(individualTeam.roster.entries.length); -> number of players on team
       for (var i = 0; i < individualTeam.roster.entries.length; i++) { 
         var individualPlayer = individualTeam.roster.entries[i].playerPoolEntry.player;
-        // console.log(individualPlayer.fullName + " " + individualPlayer.id);
+        console.log(individualPlayer.fullName + " " + individualPlayer.id);
       };
     });
 
