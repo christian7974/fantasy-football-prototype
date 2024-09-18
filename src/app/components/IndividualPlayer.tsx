9008bc39bb270900cb68d6bdd396822fd8edf61a
christian7974 <ctropeano@uri.edu>
2024-09-18 11:33:45 -0400

    Added a Placeholder Image for Players
    Added a placeholder image for players so that the roster div does not shrink and grow as a player's picture is loaded.

--- src/app/components/IndividualPlayer.tsx
+++ src/app/components/IndividualPlayer.tsx
@@ -1,16 +1,26 @@
 "use client";
 
+import { useState } from "react";
+
 import { Player } from "../types";
 
 import { getDefensePhoto, getPosition } from "../utils/individualPlayerUtils";
 
 export default function IndividualPlayer({player}: {player: Player}) { 
-    const playerImage = player.positionId != 16 
-        ? `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${player.id}.png` : 
-        `https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/${getDefensePhoto(player.name.split(" ")[0])}.png`;
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+    const playerImage = player.positionId != 16 ? 
+        `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${player.id}.png` 
+        : `https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/${getDefensePhoto(player.name.split(" ")[0])}.png`;
+
+    const placeholderImage = "https://a.espncdn.com/combiner/i?img=/games/lm-static/ffl/images/nomug.png&w=96&h=70&cb=1";
     return (
         <div className="bg-green-300 text-center flex text-black">
-            <img src={playerImage} className="h-20"></img>
+            <img 
+                src={isImageLoaded ? playerImage : placeholderImage} 
+                className="h-20"
+                onLoad={() => setIsImageLoaded(true)}
+            />
             <h1 className="">{player.name}</h1>
             {player.injuryStatus != "N/A" && <h1 className="">{player.injuryStatus}</h1>}
             <h1>{getPosition(player.positionId)}</h1>
--- src/app/components/IndividualRoster.tsx
+++ src/app/components/IndividualRoster.tsx
@@ -5,7 +5,7 @@ import IndividualPlayer from "./IndividualPlayer";
 export default function IndividualRoster({roster}: {roster: Player[]}) { 
     roster.sort((a, b) => a.positionInLineup - b.positionInLineup);
     return (
-    <div className="bg-green-300 w-fit text-center">
+    <div className="bg-green-300 w-[20%] text-center">
         {roster.map(player => (
             <IndividualPlayer player={player} key={player.id} />
         ))}
